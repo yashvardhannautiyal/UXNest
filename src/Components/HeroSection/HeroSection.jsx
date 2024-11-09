fced6e650e882f838ad916d4e0bb549c79713096
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-11-09 12:26:56 +0530

    hero section edited and logos added

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,31 +1,72 @@
 import { useNavigate } from "react-router-dom";
-const HeroSection = ()=>{
-
-    const navigate = useNavigate();
-    const handleGetStartedBtn = () =>{
-        navigate("/editor");
-    }
-    return (
-        <>
-        <div className="main-container text-purple-100">
+const HeroSection = () => {
+  const navigate = useNavigate();
+  const handleGetStartedBtn = () => {
+    navigate("/editor");
+  };
+  return (
+    <>
+      {/* section 1 */}
+      <div className="main-container text-purple-100">
         <div className="md:flex md:justify-center md:p-5 mt-5">
-        <div className="md:text-8xl text-5xl font-medium p-3">        
-        <p className="md:text-center">Build Better, Compile Faster.</p>
-        </div>
+          <div className="md:text-8xl text-5xl font-medium p-3">
+            <p className="md:text-center">Build Better, Compile Faster.</p>
+          </div>
         </div>
         <div className="md:font-normal font-medium md:text-lg w-auto flex md:text-center pb-2 md:justify-center">
-            <p className="p-3 md:w-2/3">UXNest is a powerful, intuitive code editor to revolutionize your development workflow with our front-end compiler. Fast, efficient, and powerful. Transform your code into flawless applications with zero hassle.</p>
+          <p className="p-3 md:w-2/3">
+            UXNest is a powerful, intuitive code editor to revolutionize your
+            development workflow with our front-end compiler. Fast, efficient,
+            and powerful. Transform your code into flawless applications with
+            zero hassle.
+          </p>
         </div>
-        <div className="flex md:justify-center md:pl-0">        
-        <button className="bg-purple-600 text-white rounded-lg m-1 p-1  font-medium lg:w-1/4 md:w-2/3 w-full hover:bg-purple-500 transition delay-2000 duration-300"
-        onClick={handleGetStartedBtn}>Get Started</button>
+        <div className="flex md:justify-center md:pl-0">
+          <button
+            className="bg-purple-600 text-white rounded-md m-1 p-1  font-medium lg:w-1/4 md:w-2/3 w-full hover:bg-purple-500 transition delay-2000 duration-300"
+            onClick={handleGetStartedBtn}
+          >
+            Get Started
+          </button>
         </div>
 
-        <div className="md:p-4 mt-16">
-            <p className="md:text-6xl text-4xl font-medium md:p-9 md:m-6 md:text-center p-3">Compile Like a Pro, Code Like a Boss.</p>
+
+        {/* section 2 */}
+        <div className="md:p-4 mt-10">
+          <p className="md:text-6xl text-4xl font-medium md:p-9 md:m-6 md:text-center p-3">
+            Compile Like a Pro, Code Like a Boss.
+          </p>
+
+          <div className="bg-black bg-opacity-35 rounded-lg flex items-center justify-center p-9">
+            <div className="flex p-4">
+            {/* html-logo  */}
+            <div className="p-5">
+            <img src="../Assets/htmlLogo.png" alt="" className="h-24" />
+            </div>
+            {/* css-logo  */}
+            <div className="p-5">
+            <img src="../Assets/cssLogo.png" alt="" className="h-24"/>
+            </div>
+            {/* js-logo  */}
+            <div className="p-5">
+            <img src="../Assets/jsLogo.png" alt="" className="h-24"/>
+            </div>
+            </div>
+
+            <div className="w-3/5">
+                <h1 className="font-extrabold text-3xl w-2/3">
+                Bring Your Vision to Life – Transform Ideas into Stunning Front-End Experiences!
+                </h1>
+                <p className="text-lg font-medium">Turn your ideas into beautiful, engaging front-end designs that captivate and connect with your audience. Bring your vision to life with stunning, user-friendly web experiences!</p>
+            </div>
+          </div>
         </div>
-        </div>
-        </>
-    )
-}
+
+
+
+
+      </div>
+    </>
+  );
+};
 export default HeroSection;
