aa4edc3685b6e95ba181e0421bc5b4e1bd773f68
Vaishnaviv114 <vaishnaviverma3452@gmail.com>
2024-11-30 13:06:27 +0530

    error catching resolved

--- src/Components/Editor/ErrorBoundary.jsx
+++ src/Components/Editor/ErrorBoundary.jsx
@@ -16,7 +16,7 @@ class ErrorBoundary extends Component {
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+      return <h1>Something went wrong in the JavaScript execution.</h1>;
     }
 
     return this.props.children; 
